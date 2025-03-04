7fdf549a4d94ec5331eab41ff25433e506bc488a
Amyatul Janah <chillabaldwin@gmail.com>
2025-03-04 15:21:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings: ["Mobile App Design" , "UI/UX Design"  , "Back-end Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
